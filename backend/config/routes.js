94c601b5f8403964cacfb201aa2e421083a58452
Gian10 <gianaraujo@hotmail.com>
2020-10-26 17:38:08 -0300

    service user

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -3,4 +3,9 @@ module.exports = app =>{
     // users
     app.route('/user')
         .post(app.services.userService.save)
+       
+
+    app.route('/user/:id')
+        .put(app.services.userService.save)
+        .get(app.services.userService.getById)
 }
--- backend/services/userService.js
+++ backend/services/userService.js
@@ -7,21 +7,34 @@ module.exports = app =>{
     }
 
     const save = (req, res) =>{
-        const use = { ...req.body}
+        const user = { ...req.body}
 
         if(req.params.id){
-            use.id = req.params.id
+            user.id = req.params.id
         }
 
-        use.senha = encryptPassword(req.body.senha)
+        user.senha = encryptPassword(req.body.senha)
 
-        if(use.id){
-            
+        if(user.id){
+           app.db('users').update(user).where({id: user.id})
+           .then(_ => res.status(201).send('Ataualizado'))
+           .catch(erro => res.status(500).send(erro))
+
         }else{
-
+            app.db('users').insert(user)
+            .then(_ => res.status(201).send('salvo'))
+            .catch(erro => res.status(500).send(erro))
         }
         
     }
 
-    return {save}
+    const getById = (req, res) =>{
+        app.db('users').select('id', 'nome', 'senha').where({id: req.params.id})
+        .then(user => res.status(200).send(user))
+        .catch(erro => res.status(500).send("Erro ao pesquisar usuário"))
+    }
+
+   
+
+    return {save, getById}
 }
