40c8ef4f396442542f11c710aa0ce550e3c83eb2
Gian10 <gianaraujo@hotmail.com>
2020-10-29 15:52:17 -0300

    service product

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -11,14 +11,19 @@ module.exports = app =>{
     // cad user
     app.route('/login/cadastroUser')
         .post(app.services.userService.save)
-
-
     // users
     // app.route('/user')
     //     .post(app.services.userService.save)
-       
     app.route('/user/:id')
         .all(app.config.passport.authenticate())
         .put(app.services.userService.save)
         .get(app.services.userService.getById)
+
+    app.route('/product')
+        .post(app.services.productService.save)
+        .get(app.services.productService.getProduct)
+
+    app.route('/product/:id')
+        .get(app.services.productService.getByIdProduct)
+        .delete(app.services.productService.deleteProduct)
 }
--- backend/migrations/20201028194549_create_table_product.js
+++ backend/migrations/20201028194549_create_table_product.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('product' ,table =>{
+        table.increments('id').primary()
+        table.string('nomeProduto').notNull()
+        table.string('codigo').notNull()
+        table.float('precoCusto').notNull()
+        table.float('precoVenda').notNull()
+        table.integer('estoque').notNull()
+    })
   
 };
 
 exports.down = function(knex) {
+    return knex.schema.dropTable('product')
   
 };
