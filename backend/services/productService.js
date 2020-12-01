53965c6cb34f71173977f54d2565c3ac483fbecf
Gian10 <gianaraujo@hotmail.com>
2020-12-01 14:20:41 -0300

    pesquisa de produto com paginação

--- backend/services/productService.js
+++ backend/services/productService.js
@@ -11,11 +11,11 @@ module.exports = app =>{
         if(product.produto_id){
             app.db('product').update(product).where({produto_id: product.produto_id})
             .then(_ => res.status(201).send())
-            .catch(erro => res.status(500).send('erro ao atualizar'))
+            .catch(erro => res.status(500).send('ERRO AO ATUALIZAR PRODUTO'))
         }else{
             app.db('product').insert(product)
             .then(product => res.status(201).send())
-            .catch(erro => res.status(500).send('erro ao salvar'))
+            .catch(erro => res.status(500).send('ERRO AO SALVAR PRODUTO'))
         }
     }
 
@@ -23,6 +23,7 @@ module.exports = app =>{
         const products = await app.db('product')
         .select('produto_id', 'nome_produto', 'codigo_produto', 'preco_custo', 'preco_venda', 'estoque')
         .orderBy('produto_id').limit(10).offset((req.query.page - 1) * 10)
+        .catch(_ => res.status(500).send('ERRO AO BUSCAR PRODUTOS'))
 
         const countProduct = await app.db('product').count()
 
@@ -31,28 +32,37 @@ module.exports = app =>{
             'products' : products
         } 
         res.send(productLength)
-    }
-
-    
+    } 
 
     const getByIdProduct = (req, res) =>{
         app.db('product')
         .select('produto_id', 'nome_produto', 'codigo_produto', 'preco_custo', 'preco_venda', 'estoque')
         .where({produto_id: req.params.produto_id})  
         .then(product => res.status(200).send(product))
-        .catch(erro => res.status(500).send('erro ao buscar produto por id'))      
+        .catch(erro => res.status(500).send('ERRO AO BUSCAR PRODUTO PELO ID'))      
     }
 
     const deleteProduct = async (req, res) =>{
         app.db('product').delete().where({produto_id: req.params.produto_id})
         .then(_ => res.status(200).send())
-        .catch(_ => res.status(500).send('erro ao deletar produto'))
+        .catch(_ => res.status(500).send('ERRO AO DELETAR PRODUTO PELO ID'))
     }
 
-    const searchProduct = (req, res) =>{
-        app.db('product').where('nome_produto', 'like', `%${req.query.product}%`)
-        .then(user => res.status(200).send(user))
-        .catch(_ => res.status(500).send('erro'))
+    const searchProduct = async (req, res) =>{
+        const productSearch = await app.db('product').where('nome_produto', 'like', `%${req.query.product}%`)
+        .limit(10).offset((req.query.page - 1) * 10)
+        .catch(_ => res.status(500).send('ERRO AO BUSCAR PESQUISA DE PRODUTO'))
+
+        const countSearchProduct = await app.db('product').count().where('nome_produto', 'like', `%${req.query.product}%`)
+        .catch(_ => res.status(500).send('ERRO AO BUSCAR O TAMANHO DA PESQUISA'))
+
+        const searchProductCount = {
+            'countProducts' : countSearchProduct[0].count,
+            'products' : productSearch
+        }
+        
+        res.status(200).send(searchProductCount)
+       
     }
 
     return {saveProduct, getProducts, getByIdProduct, deleteProduct, searchProduct}
--- backend/services/sumValueService.js
+++ backend/services/sumValueService.js
@@ -5,13 +5,13 @@ module.exports = app=>{
     const sumTotalOutput = async (req, res) =>{
         await app.db.raw(query.somaSaida)
         .then(value => res.status(200).send(value.rows[0]))
-        .catch(erro => res.status(500).send("erro ao somar saída"))
+        .catch(erro => res.status(500).send('ERRO AO SOMAR DADOS DE SAÍDA'))
     }
 
     const sumTotalInput = async (req, res)=>{
         await app.db.raw(query.somaEntrada)
         .then(sum => res.status(200).send(sum.rows[0]))
-        .catch(erro => res.status(500).send('erro ao somar entrada'))
+        .catch(erro => res.status(500).send('ERRO AO SOMAR DADOS DE ENTRADA'))
     }
     return {sumTotalOutput, sumTotalInput}
 }
